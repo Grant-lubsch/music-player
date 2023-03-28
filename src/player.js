1bbe3df77f215291ca689d197bebd35b7f611c76
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-28 10:47:49 -0500

    added player component

--- src/player.js
+++ src/player.js
@@ -4,3 +4,43 @@ import qaka from "../assets/qala.mp3";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { IconContext } from "react-icons";
+
+export default function Player() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [time, setTime] = useState({
+        min: "",
+        sec: ""
+    });
+    const [currTime, setCurrTime] = useState({
+        min: "",
+        sec: ""
+});
+
+const [seconds, setSeconds] = useState();
+
+const [play, { pause, duration, sound }] = useSound(qala);
+
+useEffect(() => {
+    if (duration) {
+        const sec = duration / 1000;
+        const min = Math.floor(sec / 60);
+        const secRemian = Math.floor(sec % 60);
+        setTime({
+            min: min,
+            sec: secRemain
+        });
+    }
+}, [isPlaying]);
+
+useEffect(() => {
+    const interval = setInterval(() =>
+    if (sound) {
+        setSeconds(sound.seek([]));
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+            min,
+            sec
+        });
+    }
+}, [sound]);
