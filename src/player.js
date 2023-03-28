31320ebfee1424e7f7b2628bf7fef034488c2f53
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-28 11:08:36 -0500

    added render functions

--- src/player.js
+++ src/player.js
@@ -33,7 +33,7 @@ useEffect(() => {
 }, [isPlaying]);
 
 useEffect(() => {
-    const interval = setInterval(() =>
+    const interval = setInterval(() => {
     if (sound) {
         setSeconds(sound.seek([]));
         const min = Math.floor(sound.seek([]) / 60);
@@ -43,4 +43,55 @@ useEffect(() => {
             sec
         });
     }
+}, 1000);
+return () => clearInterval(interval);
 }, [sound]);
+
+const playingButton = () => {
+    if (isPlaying) {
+        pause();
+        setIsPlaying(false);
+    } else {
+        play();
+        setIsPlaying(true);
+    }
+};
+
+return (
+    <div className="component">
+        <h2>Playing Now</h2>
+        <img className="musicCover" src="https://picsum.photos/200/200" />
+        <div>
+            <h3 className="title">Title Here</h3>
+            <p className="subTitle">Artist Here</p>
+        </div>
+        <div>
+            <div className="time">
+                <p>
+                    {currTime.min}:{currTime.sec}
+                </p>
+                <p>
+                    {time.min}:{time.sec}
+                </p>
+            </div>
+            <input
+            type="range"
+            min="0"
+            max={duration / 1000}
+            default="0"
+            value={seconds}
+            className="timeline"
+            onChange={(e) => {
+                sound.seek([e.target.value]);
+            }}
+            />
+        </div>
+        <div>
+            <button className="playButton">
+                <IcontContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+                    
+                </IcontContext.Provider>
+            </button>
+        </div>
+    </div>
+)
