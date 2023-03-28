9861832232673e912dd011eef271efd171c4a305
Grant Lubsch <grant.lubsch@gmail.com>
2023-03-28 11:37:53 -0500

    fixed bugs

--- src/player.js
+++ src/player.js
@@ -6,92 +6,111 @@ import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { IconContext } from "react-icons";
 
 export default function Player() {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [time, setTime] = useState({
-        min: "",
-        sec: ""
-    });
-    const [currTime, setCurrTime] = useState({
-        min: "",
-        sec: ""
-});
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState({
+    min: "",
+    sec: "",
+  });
+  const [currTime, setCurrTime] = useState({
+    min: "",
+    sec: "",
+  });
 
-const [seconds, setSeconds] = useState();
+  const [seconds, setSeconds] = useState();
 
-const [play, { pause, duration, sound }] = useSound(qala);
+  const [play, { pause, duration, sound }] = useSound(qala);
 
-useEffect(() => {
+  useEffect(() => {
     if (duration) {
-        const sec = duration / 1000;
-        const min = Math.floor(sec / 60);
-        const secRemian = Math.floor(sec % 60);
-        setTime({
-            min: min,
-            sec: secRemain
-        });
+      const sec = duration / 1000;
+      const min = Math.floor(sec / 60);
+      const secRemian = Math.floor(sec % 60);
+      setTime({
+        min: min,
+        sec: secRemain,
+      });
     }
-}, [isPlaying]);
+  }, [isPlaying]);
 
-useEffect(() => {
+  useEffect(() => {
     const interval = setInterval(() => {
-    if (sound) {
+      if (sound) {
         setSeconds(sound.seek([]));
         const min = Math.floor(sound.seek([]) / 60);
         const sec = Math.floor(sound.seek([]) % 60);
         setCurrTime({
-            min,
-            sec
+          min,
+          sec,
         });
-    }
-}, 1000);
-return () => clearInterval(interval);
-}, [sound]);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
 
-const playingButton = () => {
+  const playingButton = () => {
     if (isPlaying) {
-        pause();
-        setIsPlaying(false);
+      pause();
+      setIsPlaying(false);
     } else {
-        play();
-        setIsPlaying(true);
+      play();
+      setIsPlaying(true);
     }
-};
+  };
 
-return (
+  return (
     <div className="component">
-        <h2>Playing Now</h2>
-        <img className="musicCover" src="https://picsum.photos/200/200" />
-        <div>
-            <h3 className="title">Title Here</h3>
-            <p className="subTitle">Artist Here</p>
+      <h2>Playing Now</h2>
+      <img className="musicCover" src="https://picsum.photos/200/200" />
+      <div>
+        <h3 className="title">Title Here</h3>
+        <p className="subTitle">Artist Here</p>
+      </div>
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {time.min}:{time.sec}
+          </p>
         </div>
-        <div>
-            <div className="time">
-                <p>
-                    {currTime.min}:{currTime.sec}
-                </p>
-                <p>
-                    {time.min}:{time.sec}
-                </p>
-            </div>
-            <input
-            type="range"
-            min="0"
-            max={duration / 1000}
-            default="0"
-            value={seconds}
-            className="timeline"
-            onChange={(e) => {
-                sound.seek([e.target.value]);
-            }}
-            />
-        </div>
-        <div>
-            <button className="playButton">
-                <IcontContext.Provider value={{ size: "3em", color: "#27AE60" }}>
-                    
-                </IcontContext.Provider>
-            </button>
-        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
+      </div>
+      <div>
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipPrevious />
+          </IconContext.Provider>
+        </button>
+        {!isPlaying ? (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPlayCircle />
+            </IconContext.Provider>
+          </button>
+        ) : (
+          <button className="playButton" onClick={playingButton}>
+            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+              <AiFillPauseCircle />
+            </IconContext.Provider>
+          </button>
+        )}
+        <button className="playButton">
+          <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+            <BiSkipNext />
+          </IconContext.Provider>
+        </button>
+      </div>
     </div>
-)
+  );
+}
